9de95839cf1cdc33add64f657a15aafe0a5985e8
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-04 09:36:34 +0300

    add Enum class for commands and parseArgs method

--- jit.py
+++ jit.py
@@ -4,5 +4,30 @@ By - Dagim G. Astatkie (dagimg-dot)
 Wednesday, October 04 2023
 """
 import sys
+import enum
 
-print(sys.argv)
+args = sys.argv
+Enum = enum.Enum
+
+
+class Commands(Enum):
+    """
+    Enum class for all available commands
+    """
+    LOCAL_REMOTE = "local-remote"
+    PULL = "pull"
+    PUSH = "push"
+
+
+ARG1 = [command.value for command in Commands]
+
+
+def parseArgs():
+    if len(args) == 1:
+        print("JIT - One step away to collaborate with others offline using git")
+    else:
+        if args[1] not in ARG1:
+            print(f"Unknown command '{args[1]}'")
+
+
+parseArgs()
